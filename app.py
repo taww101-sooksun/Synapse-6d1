2ef4975684d1ad4106ae81162d01203093331c6b
taww101-sooksun <taww101@gmail.com>
2026-02-11 12:49:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def generate_hiphop_beat(duration=180):
     sr = 44100
     t = np.linspace(0, duration, int(sr * duration), False)
     tempo = 90  # Beats per minute
-    beat_duration = 60 / tempo
+    beat_duration = 40 / tempo
     
     # 1. Sub-Bass 147Hz (เตะตามจังหวะ Kick)
     kick_pattern = np.zeros_like(t)
