e3ee2104a765f3b66bd2f328ce93af8dcbf5a13f
taww101-sooksun <taww101@gmail.com>
2026-04-14 20:23:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-st.image("static/my_image.png")
+import streamlit as st
+from PIL import Image
+import streamlit as st
+
+# ใส่ URL ของรูปภาพที่ต้องการ
+st.image("https://example.com", caption="คำอธิบายใต้ภาพ")
+
+# เปิดไฟล์ภาพที่อยู่ในโฟลเดอร์เดียวกับโค้ด
+image = Image.open('my_picture.png')
+st.image(image, caption='รูปภาพจากเครื่อง', use_container_width=True)
